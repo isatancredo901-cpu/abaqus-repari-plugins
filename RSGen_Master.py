77eaf9e8b5b6320e661c812a86837c758cbc596a
isatancredo901-cpu <isatancredo901-cpu@users.noreply.github.com>
2026-06-24 23:00:48 +0800

    Guard against degenerate cohesive thickness

--- RSGen_Master.py
+++ RSGen_Master.py
@@ -14,6 +14,8 @@ from driverUtils import executeOnCaeStartup
 executeOnCaeStartup()
 import json
 
+MIN_COHESIVE_GEOMETRY_THICKNESS = 0.001
+
 
 def exec_script(path, namespace=None):
     if namespace is None:
@@ -103,12 +105,25 @@ def applyBC():
 def string2list(string):
     return [float(i) for i in string.split("/")]
 
+
+def parse_json_dict(value):
+    if isinstance(value, dict):
+        return value
+    if value is None:
+        return {}
+    value = value.strip()
+    if not value:
+        return {}
+    return json.loads(value)
+
+
 def execute(pluginDir='C:\\Users\\Binz\\Desktop\\Undelivered _pro\\prototypeApp', cmdType='createModel', mLength=150, mWidth=100, mHeight=1.025, mThickness=0.125, mNumLayer=5, mLayerAngle='45/0/-45/90/45/0/-45/90/45/0/-45/90/45/0/-45/90/90/-45/0/45/90/-45/0/45/90/-45/0/45/90/-45/0/45', mPitDiameter=10, mMaterialFileKw='C:/Users/Binz/Desktop/Undelivered _pro/prototypeApp/material.json', mOdbFileKw='', modifyType=0, tbWidth=20, tbHeight=20, tbLayerNum=4, tbLayerAngle='0/10/20/40', glueThickness=0.00001, jtNum=4, xjAngle=10, lrGap=1.2, tbGap=1.2, disValue=1000, jobName='Job-1', numOfCpus=1, mSubroutineFileKw='', parseOdbType=0, tableKw=(), matParaDict='{"material":{"adhesive":{"power":1.45,"de":[2.058,2.563,2.563],"qd":[122,136,136],"elastic":[100000,100000,100000],"density":1.2e-09},"base":{"depvar":90,"data":[126000,10400,10400,0.312,0.312,0.45,4500,4500,3280,0,0.06,1,1,1,1,1,1,1,2076,1357,50.9,187,55.4,150,1,1,66,66,66,1,1,1,1,1,20,10,0.1,1,1],"density":1.522e-09},"cohesiveZero":{"power":1.45,"de":[2.058,2.563,2.563],"qd":[122,136,136],"elastic":[100000,100000,100000],"density":1.2e-09}}}', tbType=0, tbShape=0, pressBC=0):
        # 构建 modelData 字典
     layerAngle = string2list(mLayerAngle)
     tbLayerAngle = string2list(tbLayerAngle)
     
-    matParaDict = json.loads(matParaDict)
+    matParaDict = parse_json_dict(matParaDict)
+    geometryGlueThickness = max(glueThickness, MIN_COHESIVE_GEOMETRY_THICKNESS)
 
     modelData = {
     "tbLayerAngle":tbLayerAngle,
@@ -125,14 +140,16 @@ def execute(pluginDir='C:\\Users\\Binz\\Desktop\\Undelivered _pro\\prototypeApp'
     "lrGap":lrGap,
     "tbGap":tbGap,
     "thickness":mThickness, #铺层厚度
-    "cohesive":glueThickness, #是否内聚力层"
+    "inputCohesive":glueThickness,
+    "cohesive":geometryGlueThickness, #是否内聚力层"
     "layers":layerAngle,
     "r": mPitDiameter, #缺陷半径
     "numOfCpus":numOfCpus,
     "mSubroutineFileKw":mSubroutineFileKw,
     "modify":{
         "type":modifyType, #0:贴片 1：斜接式 2:阶梯型
-        "cohesive":glueThickness, #胶接厚度
+        "inputCohesive":glueThickness,
+        "cohesive":geometryGlueThickness, #胶接厚度
         "tbShape":tbShape,
         "tbType":tbType,
 
